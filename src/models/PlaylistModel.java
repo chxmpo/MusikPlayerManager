4f61af10147dc84cc8673113b876919d2ae03bb4
chxmpo <niklasredetzki@gmail.com>
2025-10-23 09:29:52 +0200

    PlaylistModel + Song initialized

---
+++ src/models/PlaylistModel.java
@@ -0,0 +1,50 @@
+package models;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class PlaylistModel {
+    private ArrayList<Song> songs = new ArrayList<>();
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+
+    public PlaylistModel(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+
+    //neuen Song adden
+    public void addSong(Song s){
+        songs.add(s);
+    }
+
+    //song nach Index löschen
+    public void removeSong(int index){
+        if (index >= 0 && index < songs.size()) {
+            songs.remove(index);
+        }
+    }
+
+    //Song und Artist in die Datei einlesen
+    public void saveToFile(String filename){
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("SongSpeichern.txt"))) {
+            out.writeObject(songs);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadFromFile(String filename) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("SongSpeichern.txt"))) {
+            songs = (ArrayList<Song>) in.readObject();
+        } catch (Exception e) {
+            System.out.println("Keine gespeicherte Datei gefunden - neue Playlist wird erstellt.");
+            songs = new ArrayList<>();
+        }
+    }
+}
---
+++ src/models/Song.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Song {
+    private String title;
+    private String artist;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Song(){
+
+    }
+
+    public String toString(){
+        return this.getTitle()+ " - " + this.getArtist();
+    }
+}
