dc4fe9e2f4f110cc2ff1e8ed6e05bf225afcef8e
chxmpo <niklasredetzki@gmail.com>
2025-10-23 10:33:27 +0200

    +PlaylistPanel

--- src/views/PlaylistPanel.java
+++ src/views/PlaylistPanel.java
@@ -1,4 +1,72 @@
 package views;
 
-public class PlaylistPanel {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class PlaylistPanel extends JLabel {
+    private JTable table;
+    private DefaultTableModel model;
+    private JButton btnAdd, btnRemove, btnSave;  //mehrere gleichzeitig damit weniger code
+
+
+    public JButton getBtnAdd() {
+        return btnAdd;
+    }
+
+    public void setBtnAdd(JButton btnAdd) {
+        this.btnAdd = btnAdd;
+    }
+
+    public JButton getBtnRemove() {
+        return btnRemove;
+    }
+
+    public void setBtnRemove(JButton btnRemove) {
+        this.btnRemove = btnRemove;
+    }
+
+    public JButton getBtnSave() {
+        return btnSave;
+    }
+
+    public void setBtnSave(JButton btnSave) {
+        this.btnSave = btnSave;
+    }
+
+    public DefaultTableModel getModel() {
+        return model;
+    }
+
+    public void setModel(DefaultTableModel model) {
+        this.model = model;
+    }
+
+    public JTable getTable() {
+        return table;
+    }
+
+    public void setTable(JTable table) {
+        this.table = table;
+    }
+
+    public PlaylistPanel(){
+        setLayout(new BorderLayout());
+
+        //Tabellen Format
+        model = new DefaultTableModel(new Object[]{"Titel", "Künstler"}, 0);
+        table = new JTable(model);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        //Button Layout
+        JPanel buttonPanel = new JPanel();
+        btnAdd = new JButton("Song hinzufügen");
+        btnRemove = new JButton("Song löschen");
+        btnSave = new JButton("Speichern");
+        buttonPanel.add(btnAdd);
+        buttonPanel.add(btnRemove);
+        buttonPanel.add(btnSave);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
 }
