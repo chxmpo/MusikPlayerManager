6eb472ed6cf1dc074b68250b79b46392d6949067
chxmpo <niklasredetzki@gmail.com>
2025-10-23 10:39:34 +0200

    +MainFrame

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -1,4 +1,27 @@
 package views;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    private PlaylistPanel pPanel;
+
+    public PlaylistPanel getpPanel() {
+        return pPanel;
+    }
+
+    public void setpPanel(PlaylistPanel pPanel) {
+        this.pPanel = pPanel;
+    }
+
+    public MainFrame() {
+         setTitle("\uD83D\uDD25 Musik-Playlist-Manager");
+         setSize(600,400);
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setLocationRelativeTo(null);
+
+         pPanel = new PlaylistPanel();
+         add(pPanel);
+    }
+
 }
